69ab0fc7fe6e693de683657b219c4f1843fcc44d
Flavio Lara <flavio.lara@gmail.com>
2023-08-29 15:36:42 -0300

    Teste async await c#

---
+++ AsyncAwait/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AsyncAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMainTask = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMainTask
+            // 
+            this.btnMainTask.Location = new System.Drawing.Point(59, 64);
+            this.btnMainTask.Name = "btnMainTask";
+            this.btnMainTask.Size = new System.Drawing.Size(181, 23);
+            this.btnMainTask.TabIndex = 0;
+            this.btnMainTask.Text = "Long Task (using async)";
+            this.btnMainTask.UseVisualStyleBackColor = true;
+            this.btnMainTask.Click += new System.EventHandler(this.btnMainTask_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Single Log entry (NLog)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(59, 102);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(181, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel Long Task";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(491, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Count = ";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(545, 117);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMainTask);
+            this.Name = "Form1";
+            this.Text = "cd ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMainTask;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ AsyncAwait/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class Form1 : Form
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnMainTask_Click(object sender, EventArgs e)
+        {
+            //List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 10000; i++)
+            {
+                int threadID = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+                Logger.Info($"{i} - {threadID} - Primeira mensagem");
+
+                // Se usar await, as mensagens ficam todas sequenciadas
+                // Se não usar as mensagens saem fora de ordem
+                // Para gravar o arquivo teve que tratar exceção por causa de arquivo já aberto
+                //tasks.Add(DoSomething(i));
+                await DoSomething(i);
+                //tasks.Add(DoSomething(i));
+
+                Logger.Info($"{i} - {threadID} - Segunda mensagem");
+
+                lblCount.Text = i.ToString();
+
+                if (cts.Token.IsCancellationRequested)
+                {
+                    Logger.Error($"{i} - {threadID} - Loop cancelado manualmente");
+                    break;
+                }
+            }
+            //await Task.WhenAll(tasks);
+            MessageBox.Show("Fim!");
+        }
+
+        private Task DoSomething(int i)
+        {
+            return Task.Run(() => SendMessage(i));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logger.Error("Teste msg erro");
+        }
+
+        private void SendMessage(int i)
+        {
+            Logger.Warn($"{i} - {System.Threading.Thread.CurrentThread.ManagedThreadId} - Menssagem Task");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+    }
+}
